5dbf8ecf7837505f6cf70b21473ba5ae69e45dfa
jantiks <58886410+jantiks@users.noreply.github.com>
2020-12-07 17:03:44 +0400

    storyboard is ready

---
+++ projrct10/PersonCell.swift
@@ -0,0 +1,15 @@
+//
+//  PersonCell.swift
+//  projrct10
+//
+//  Created by Tigran on 12/7/20.
+//  Copyright © 2020 Tigran. All rights reserved.
+//
+
+import UIKit
+
+class PersonCell: UICollectionViewCell {
+    @IBOutlet var name: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+}
